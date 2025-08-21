2c24f742cc3e502698f70610bb9e1f24013c3dc2
Pedro Melo <pedrolucaslopesmelo@gmail.com>
2025-08-21 19:45:47 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1, n2, n3, n4, soma;
+
+        System.out.print("Digite o primeiro numero: ");
+        n1 = sc.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        n2 = sc.nextInt();
+
+        System.out.print("Digite o terceiro numero: ");
+        n3 = sc.nextInt();
+
+        System.out.print("Digite o quarto numero: ");
+        n4 = sc.nextInt();
+
+        soma = n1 + n2 + n3 + n4;
+
+        System.out.println("A soma dos numeros eh: " + soma);
+        sc.close();
+    }
+}
